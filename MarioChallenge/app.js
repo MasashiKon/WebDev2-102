f97e70efc4b1355b677c42b1140a0f3e7de39c57
MasahiKon <konnonorth@gmail.com>
2023-01-25 22:15:18 -0800

    first commit

--- MarioChallenge/app.js
+++ MarioChallenge/app.js
@@ -1,3 +1,41 @@
+const gameConfig = {
+    score: 0,
+    maxTime: 30,
+    time: 30,
+    isPlaying: false
+}
+
+const statsText = document.createElement("h1");
+const scoreText = document.createElement("span");
+const timeText = document.createElement("span");
+scoreText.innerHTML = `Score: ${gameConfig.score}`;
+timeText.innerHTML = `Time: ${gameConfig.time}`;
+statsText.appendChild(scoreText);
+statsText.appendChild(timeText);
+statsText.style.display = "flex";
+statsText.style.justifyContent = "space-between";
+document.body.appendChild(statsText);
+
+const result = document.createElement("div");
+result.innerHTML = `
+<h1>Score: ${gameConfig.score}</h1>
+<h2>press enter to play again</h2>
+`
+result.style.textAlign = "center";
+result.style.margin = "0";
+result.style.position = "absolute";
+result.style.backgroundColor = "white";
+result.style.height = "100vh";
+result.style.width = "100vw";
+result.style.top = "0";
+result.style.left = "0";
+result.style.zIndex = "1";
+result.style.display = "none";
+result.style.flexDirection = "column";
+result.style.justifyContent = "center";
+
+document.body.appendChild(result);
+
 function isTouching(a, b) {
 	const aRect = a.getBoundingClientRect();
 	const bRect = b.getBoundingClientRect();
@@ -10,24 +48,87 @@ function isTouching(a, b) {
 	);
 }
 
+function controlTween(e){
+
+    if(gameConfig.isPlaying) {
+        if(e.key === 'ArrowDown' || e.key === 'Down'){
+            moveVertical(avatar, 50);
+        }
+
+        if(e.key === 'ArrowUp' || e.key === 'Up'){
+            moveVertical(avatar, -50);
+        }
+
+        if(e.key === 'ArrowLeft' || e.key === 'Left'){
+            avatar.style.transform = "scaleX(-1)";
+            moveHorizontal(avatar, -50);
+        }
+
+        if(e.key === 'ArrowRight' || e.key === 'Right'){
+            avatar.style.transform = "scaleX(1)";
+            moveHorizontal(avatar, 50);
+        }
+
+        if(isTouching(avatar,coin)) {
+            moveCoin();
+            scoreText.innerHTML = `Score: ${++gameConfig.score}`;
+        } 
+    } else {
+        if(e.key === "Enter") {
+            gameConfig.isPlaying = true;
+            gameConfig.score = 0;
+            gameConfig.time = gameConfig.maxTime;
+            scoreText.innerHTML = `Score: ${gameConfig.score}`;
+            timeText.innerHTML = `Time: ${gameConfig.time}`;
+            result.style.display = "none";
+        }
+    }
+}
+
+function restart(e){
+
+}
+
 const init = () => {
     //get the avatar
     // const avatar = document.querySelector(".avatar")
     //get the coin
 
-    moveCoin();
-    window.addEventListener('keyup', function(e){
-        if(e.key === 'ArrowDown' || e.key === 'Down'){
-            moveVertical(avatar, 50);
-        }
+    gameConfig.isPlaying = true;
 
-        if(isTouching(avatar,coin)) moveCoin();
-    });
+    moveCoin();
+    window.addEventListener('keyup', controlTween);
+
+    const timeCount = setInterval(() => {
+        if(gameConfig.isPlaying) {
+            timeText.innerHTML = `Time: ${gameConfig.time--}`;
+
+            if(gameConfig.time < 0) {
+                gameConfig.isPlaying = false;
+    
+                result.innerHTML = `
+                <h1>Score: ${gameConfig.score}</h1>
+                <h2>press enter to play again</h2>
+                `
+                result.style.display = "flex";
+            }
+        }
+    }, 1000);
 }
+
 
 const moveVertical = (element, amount) => {
     const currTop = extractPos(element.style.top);
-    element.style.top = `${currTop + amount}px`;
+    if(currTop + amount > -50 && currTop + amount < innerHeight - 100){
+        element.style.top = `${currTop + amount}px`;
+    }
+}
+
+const moveHorizontal = (element, amount) => {
+    const currLeft = extractPos(element.style.left);
+    if(currLeft + amount > -50 && currLeft + amount < innerWidth - 100){
+        element.style.left = `${currLeft + amount}px`;
+    }
 }
 
 const extractPos = (position) => {
@@ -36,10 +137,11 @@ const extractPos = (position) => {
 }
 
 const moveCoin = () => {
-    const x = Math.floor(Math.random() * window.innerWidth)
-    // const y = ?
-    coin.style.top = `${x}px`;
-    // coin.style.?? = ??
+    const x = Math.floor(Math.random() * (innerWidth - 100))
+    const y = Math.floor(Math.random() * (innerHeight - 100))
+    coin.style.top = `${y}px`;
+    coin.style.left = `${x}px`;
 }
 
 init();
+
